1d202080d4bdaf97a378b44b6d8f21570cae57a7
Fathul Amin <116142085+famintech@users.noreply.github.com>
2024-07-18 14:27:57 +0800

    Update Header.jsx and create Nav.jsx
    Add desktop nav and hire me button

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,19 +1,29 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 
+// COMPONENTS
+import Nav from "./Nav";
+
 const Header = () => {
-  return (
-    <header className="py-8 xl:py-12 text-white">
-      <div className="container mx-auto">
-        {/* {logo} */}
-        <Link href="/">
-          <h1 className="text-4xl font-semibold">
-            Fathul Amin <span className="text-accent">.</span>
-          </h1>
-        </Link>
-      </div>
-    </header>
-  );
+    return (
+        <header className="py-8 xl:py-12 text-white">
+            <div className="container mx-auto">
+                {/* {logo} */}
+                <Link href="/">
+                    <h1 className="text-4xl font-semibold">
+                        Fathul Amin <span className="text-accent">.</span>
+                    </h1>
+                </Link>
+                {/* { desktop nav & hire me button } */}
+                <div className="hidden xl:flex">
+                    <Nav></Nav>
+                    <Link href="/contact">
+                        <Button>Hire Me</Button>
+                    </Link>
+                </div>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
